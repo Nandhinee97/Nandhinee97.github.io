4e48819bc7323857f1f656059fe44f10cf88a71f
Nandhinee97 <55523911+Nandhinee97@users.noreply.github.com>
2019-10-14 12:15:40 +0530

    portfolio upload

---
+++ test.js
@@ -0,0 +1,10 @@
+                                                                Sample BIO-DATA
+Name	:	Nandhinee
+Father Name	:	Kanniappan
+Mother Name	:	Latha
+Date of Birth	:	13-07-1997
+Qualification	:	B.E
+Address	:Chennai
+Mobile Number	:1234567789
+ 
+  
